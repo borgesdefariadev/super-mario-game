d0d2da2ad54587e6bc6aa2e80fec69a2fa448d49
Ricardo Borges de Faria <ricardoborgesdefariadev@gmail.com>
2023-10-04 14:07:22 -0300

    adicionado contador de score e estilo do game over

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,9 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const floor = document.querySelector('.floor');
+const scoreBoard = document.getElementById('score-board');
+
+let score = 0;
 
 const jump = (event) => {
   if (event.keyCode === 32) {
@@ -10,5 +15,31 @@ const jump = (event) => {
     }, 1000)
   }
 }
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  if (pipePosition > -69) {
+    score++;  
+
+    scoreBoard.textContent = `Score: ${score}`;
+  }
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 90 &&pipePosition) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`
+
+    mario.src = './assets/game-over.png';
+    mario.style.width = '75px';
+    mario.style.margin = '50px';
+    floor.src = './assets/static-floor.png';
+
+    clearInterval(loop);
+  }
+}, 10)
+
 
 document.addEventListener('keydown', jump);
